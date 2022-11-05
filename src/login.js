03e07d0749615c513c16effa36e2430d16533dc5
v3locity47 <aryanmishra530@gmail.com>
2022-11-06 01:34:05 +0530

    Adds Redirect Logic for Wrong Creds

--- src/login.js
+++ src/login.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
+import usersData from "./database/login.json";
 import Appointment from "./appointment";
-// import fs from "fs";
 
 const Login = () => {
   const [email, setemail] = useState("");
@@ -14,7 +14,7 @@ const Login = () => {
   const [date, setDate] = useState("");
   const [pname, setPname] = useState("");
   const [age, setAge] = useState("");
-  const [blood, setblood]= useState("");
+  const [blood, setblood] = useState("");
   const [gender, setgender] = useState("");
   const [phone, setphone] = useState("");
   // const [isloading, setisloading] = useState(false);
@@ -22,29 +22,23 @@ const Login = () => {
 
   const handlesubmit = (e) => {
     e.preventDefault();
-    let patients = {pname,blood,gender, age, date, time, phone };
+    let patients = { pname, blood, gender, age, date, time, phone };
     var login = { email, password };
     setisloading(true);
     console.log("logged in: " + login);
-
-    fetch("http://localhost:8004/patients", {
-      method: "GET",
-      body: JSON.stringify(patients),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }) .then((response) => {
-      console.log(response);
-      console.log("logged in");
-      setisloading(false);
-      // history.push("/appointment");
-    })
-    .catch((error) => {
-      console.log(error);
+    console.log(usersData);
+    const { login: loginData } = usersData;
+    const isUserPresent = loginData.find((user) => {
+      if (user.email === email) {
+        return true;
+      }
+      return false;
     });
-  
-
-
+    console.log(isUserPresent);
+    if (!isUserPresent) {
+      console.log("User Does Not Exist");
+      window.location.href = "/signup";
+    }
     fetch("http://localhost:8005/login", {
       method: "POST",
       body: JSON.stringify(login),
@@ -68,59 +62,60 @@ const Login = () => {
   };
   return (
     <div className="parentloginclass">
-    <center><h1>LOGIN FORM</h1></center>
-    <div className="Login center">
-      <Form onSubmit={handlesubmit}>
-        <Form.Group size="lg" id="em" controlId="email">
-          <Form.Label>
-            <b>EMAIL </b>
-          </Form.Label>
+      <center>
+        <h1>LOGIN FORM</h1>
+      </center>
+      <div className="Login center">
+        <Form onSubmit={handlesubmit}>
+          <Form.Group size="lg" id="em" controlId="email">
+            <Form.Label>
+              <b>EMAIL </b>
+            </Form.Label>
 
-          <Form.Control
-            autoFocus
-            required
-            className="inputsignup"
-            type="email"
-            value={email}
-            onChange={(e) => setemail(e.target.value)}
-          />
-        </Form.Group>
-
-        <Form.Group size="lg" controlId="password" className="pass">
-          <Form.Label>
-            <b>PASSWORD </b>
-          </Form.Label>
-
-          <Form.Control
-            required
-            className="inputsignup"
-            type="password"
-            value={password}
-            onChange={(e) => setpassword(e.target.value)}
-          />
-        </Form.Group>
-        <div className="signupbtn2">
-          {!isloading && (
-            <Button
+            <Form.Control
+              autoFocus
+              required
               className="inputsignup"
-              block
-              size="lg"
-              type="submit"
-              id="submitlogin"
-            >
-              Submit
+              type="email"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group size="lg" controlId="password" className="pass">
+            <Form.Label>
+              <b>PASSWORD </b>
+            </Form.Label>
+
+            <Form.Control
+              required
+              className="inputsignup"
+              type="password"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+            />
+          </Form.Group>
+          <div className="signupbtn2">
+            {!isloading && (
+              <Button
+                className="inputsignup"
+                block
+                size="lg"
+                type="submit"
+                id="submitlogin"
+              >
+                Submit
+              </Button>
+            )}
+          </div>
+          {isloading && (
+            <Button className="signupbtn2" restricted>
+              Logging In...
             </Button>
           )}
-        </div>
-        {isloading && (
-          <Button className="signupbtn2" restricted>
-            Logging In...
-          </Button>
-        )}
-      </Form>
+        </Form>
+      </div>
     </div>
-    </div>
-
   );
 };
 
